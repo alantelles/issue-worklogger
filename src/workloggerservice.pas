fbcde2b130470666840d7fcd66f1b9c3f4018e7e
Alan Telles <alantelles86@gmail.com>
2022-07-06 23:28:25 -0300

    feat: use a field in csv to mark log status

--- src/workloggerservice.pas
+++ src/workloggerservice.pas
@@ -13,17 +13,39 @@ const
   DATE_FORMAT = 'yyyy-mm-dd"T"hh:nn:ss.zzz';
   WORK_PAUSE = 'Work pause';
 
+  // Statuses
+  NOW_WORKING = 'Now working';
+  FINISHED = 'Finished';
+  SENT_TO_HUB = 'Sent';
+  SENDING = 'Sending';
+  ERROR_ON_SENDING = 'Error on sending';
+
 type
   TWorkloggerService = class
     public
       class function GetTodayWorklogPath: string;
       class procedure SaveWorklog(issue: string);
       class function GetDateStartedFromLine(Line: string): TDateTime;
+      class function GetFinishedLastIssue(Line: string): string;
   end;
 
 implementation
 uses
   DateUtils, StrUtils;
+
+class function TWorkloggerService.GetFinishedLastIssue(Line: string): string;
+var
+  TimeSpentSeconds: integer;
+  LinePart: string;
+begin
+  // IssueKey;yyyy-mm-ddThh:nn:ss.zzz+0300;STATUS;
+  TimeSpentSeconds := Trunc(SecondSpan(
+                     Now,
+                     TWorkloggerService.GetDateStartedFromLine(Line)
+                     ));
+  LinePart := Copy(Line, 1, NPos(CSV_SEPARATOR, Line, 2));
+  Result := LinePart + FINISHED + CSV_SEPARATOR + IntToStr(TimeSpentSeconds);
+end;
 
 class function TWorkloggerService.GetTodayWorklogPath: string;
 const
@@ -36,7 +58,7 @@ class function TWorkloggerService.GetDateStartedFromLine(Line: string): TDateTim
 var
   Strdate: string;
 begin
-  Strdate := Copy(Line, Pos(CSV_SEPARATOR, Line) + 1, RPos(CSV_SEPARATOR, Line) - 1);
+  Strdate := Copy(Line, Pos(CSV_SEPARATOR, Line) + 1, Length(Line));
   StrDate := Copy(StrDate, 1, Pos('+', Strdate) - 1);
   Result := ScanDateTime(DATE_FORMAT, StrDate);
 end;
@@ -49,10 +71,9 @@ var
   worklogPath: string;
   WorklogCsv: TStringList;
   StrDate: string;
-  TimeSpentSeconds: integer;
   SameIssueLast: boolean = false;
 begin
-  // issueId;started;timeSpentSeconds
+  // IssueKey;yyyy-mm-ddThh:nn:ss.zzz+0300;STATUS;
   WorklogPath := TWorkLoggerService.GetTodayWorklogPath;
   WorklogCsv := TStringList.Create;
   WorklogCsv.SkipLastLineBreak := True;
@@ -63,17 +84,16 @@ begin
     SameIssueLast := Copy(WorklogCsv[WorklogCsv.Count - 1], 1, Pos(CSV_SEPARATOR, WorklogCsv[WorklogCsv.Count - 1]) - 1) = issue;
     if not SameIssueLast then
     begin
-      TimeSpentSeconds := Trunc(SecondSpan(
-                     Now,
-                     TWorkloggerService.GetDateStartedFromLine(WorklogCsv[WorklogCsv.Count - 1])
-                     ));
-      WorklogCsv[WorklogCsv.Count - 1] := WorklogCsv[WorklogCsv.Count - 1] + IntToStr(TimeSpentSeconds);
+      WorklogCsv[WorklogCsv.Count - 1] := TWorkloggerService.GetFinishedLastIssue(WorklogCsv[WorklogCsv.Count - 1]);
     end;
   end;
   if not SameIssueLast then
   begin
     StrDate := FormatDateTime(DATE_FORMAT, Now);
-    WorklogCsv.Add(issue + CSV_SEPARATOR + StrDate + DATE_FILLER + CSV_SEPARATOR);
+    // IssueKey;yyyy-mm-ddThh:nn:ss.zzz+0300;STATUS;
+    WorklogCsv.Add(issue + CSV_SEPARATOR +
+                         StrDate + DATE_FILLER + CSV_SEPARATOR +
+                         NOW_WORKING + CSV_SEPARATOR);
     WorklogCsv.SaveToFile(worklogPath);
   end;
   WorklogCsv.Free;
