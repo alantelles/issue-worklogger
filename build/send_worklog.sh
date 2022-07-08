9a628e6cd34c2a76a4fabaad870ed5357d65ef3d
Alan Telles <alantelles86@gmail.com>
2022-07-07 21:00:42 -0300

    refactor: adjust script to send worklog

--- build/send_worklog.sh
+++ build/send_worklog.sh
@@ -13,6 +13,16 @@ STARTED_AT=$2
 # The worklog spent time in seconds
 TIME_SPENT=$3
 
+# The script needs these variables be set
+# Environment variables won't be used by this script when executed by IssueWorklogger
+# JIRA_API_KEY=your-api-key
+# JIRA_USER=your@account.email
+# JIRA_DOMAIN="your-domain.atlassian.net"
+
+# It's recommended you source a file with it
+source ~/.jira-vars.sh
+
+
 # Below parts are specific to Jira
 BODY="{\"started\":\"$STARTED_AT\",\"timeSpentSeconds\":$TIME_SPENT}"
 
--- src/workloglist.pas
+++ src/workloglist.pas
@@ -14,6 +14,7 @@ type
 
   TFrmLogList = class(TForm)
     BtnSendWorklogs: TButton;
+    BtnReloadWorklogs: TButton;
     SgrWorklogList: TStringGrid;
     StatusBar1: TStatusBar;
     procedure BtnSendWorklogsClick(Sender: TObject);
@@ -28,7 +29,7 @@ var
   FrmLogList: TFrmLogList;
 
 procedure SendWorklogs(SGrid: TStringGrid);
-procedure SendWorklog(issue: string; startedAt: string; TimeSpent: string);
+function SendWorklog(issue: string; startedAt: string; TimeSpent: string): boolean;
 
 implementation
 
@@ -41,6 +42,7 @@ uses
 
 procedure TFrmLogList.FormShow(Sender: TObject);
 begin
+  StatusBar1.SimpleText := 'Ready';
   if FileExists(TWorkloggerService.GetTodayWorklogPath) then
     SgrWorklogList.LoadFromCSVFile(TWorkloggerService.GetTodayWorklogPath, CSV_SEPARATOR, false, 0, true)
   else
@@ -61,34 +63,57 @@ end;
 
 procedure SendWorklogs(Sgrid: TStringGrid);
 var
-  i, logs: integer;
+  i, logs, sent, readytoSend, errorsOnSend: integer;
+  WorklogSaveResult: boolean;
 begin
   logs := Sgrid.RowCount - 1;
   if logs > 1 then
   begin
+    sent := 0;
+    ReadyToSend := 0;
+    errorsOnSend := 0;
     for i:= 1 to logs - 1 do
     begin
       if (SGrid.Cells[0, i] <> WORK_PAUSE) and
          ((SGrid.Cells[2, i] = FINISHED) or (SGrid.Cells[2, i] = ERROR_ON_SENDING)) then
       begin
-        SendWorklog(
+        readytoSend := readyToSend + 1;
+        WorklogSaveResult := SendWorklog(
           SGrid.Cells[0, i], // issue
           SGrid.Cells[1, i], // startedAt
           SGrid.Cells[3, i] // timespent
         );
+        if WorklogSaveResult then
+        begin
+          SGrid.Cells[2, i] := SENT_TO_HUB;
+          sent := sent + 1;
+        end
+        else
+        begin
+          SGrid.Cells[2, i] := ERROR_ON_SENDING;
+          errorsOnSend := errorsOnsend + 1;
+        end;
+        Sgrid.SaveToCSVFile(TWorkloggerService.GetTodayWorklogPath, CSV_SEPARATOR, False, True);
+        Sleep(10);
       end;
+
     end;
-    FrmLogList.StatusBar1.SimpleText := 'Finished!';
-    ShowMessage('Finished');
+    FrmLogList.StatusBar1.SimpleText := 'Finished! Ready: ' + IntToStr(readyToSend) + '; Sent: ' + IntToStr(Sent) + '; Errors: ' + IntToStr(ErrorsOnSend);
+    ShowMessage('Finished!' + sLinebreak + sLineBreak +
+        'Ready: ' + IntToStr(readyToSend) + sLineBreak +
+        'Sent: ' + IntToStr(Sent) + sLineBreak +
+        'Errors: ' + IntToStr(ErrorsOnSend)
+    );
   end
   else
     ShowMessage('No worklogs ready to be sent');
 end;
 
-procedure SendWorklog(issue: string; startedAt: string; TimeSpent: string);
+function SendWorklog(issue: string; startedAt: string; TimeSpent: string): boolean;
 begin
+  Application.ProcessMessages;
   FrmLogList.StatusBar1.SimpleText := 'Sending: ' + issue + '/' + startedAt + '/' + TimeSpent;
-  TWorkloggerService.SendWorklogToManager(issue, startedAt, timespent);
+  Result := TWorkloggerService.SendWorklogToManager(issue, startedAt, timespent);
 end;
 
 end.
